4286e757f87d19953f2f78a8d0dbfce2d99b4a8d
odiroot <odi.root@gmail.com>
2013-11-11 19:53:16 +0100

    Merged classifier parsing support, bumped up the version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='license-info',
-    version='0.8.4',
+    version='0.8.5',
     description='Show list of installed python packages with version and license info',
     author='Michal Odnous',
     author_email='odi.root@gmail.com',
